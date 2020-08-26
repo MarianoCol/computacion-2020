dc5ccf017ffa8dfd1cdc8fc5cbc37da726867ae3
Mariano Colman <marianito_1998_ma@hotmail.com>
2020-08-25 22:17:25 -0300

    TPN°6

--- TPN5/productoServices.py
+++ TPN5/productoServices.py
@@ -1,4 +1,5 @@
 from repositorios import Repositorios
+import operator
 
 class ProductoService():
     pass
@@ -19,3 +20,23 @@ class ProductoService():
 
     def get_productosList(self):
         return Repositorios.productosList
+
+    def insertion_sort_precio(self, diccionario, tipoOrden):
+        if tipoOrden == "ascendente":
+            for i in range(1, len(diccionario)):
+                #guarda el precio de la primera iteracion
+                valorPrecio = diccionario[i]['_precio']
+                #entra en el bucle si encuentra un precio mas grande que se encuentre en un indice anterior
+                while i > 0 and diccionario[i-1]['_precio'] > valorPrecio:
+                    #intercambia las posiciones 
+                    diccionario[i], diccionario[i-1] = diccionario[i-1], diccionario[i]
+                    i = i-1
+            return diccionario
+        #Realiza el mismo proceso pero a la inversa
+        for i in range(1, len(diccionario)):
+            valorPrecio = diccionario[i]['_precio']
+            #Entra en el bucle cuando encuentra un precio menor en una posicion posterior
+            while i > 0 and diccionario[i-1]['_precio'] < valorPrecio:
+                diccionario[i], diccionario[i-1] = diccionario[i-1], diccionario[i]
+                i = i-1
+        return diccionario
